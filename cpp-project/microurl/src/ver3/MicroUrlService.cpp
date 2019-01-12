4840237083e6deb2ea2a83346836fb8076c4d987
Marco Arena <ilpropheta@gmail.com>
2019-01-12 11:17:26 +0100

    Update MicroUrlService.cpp

--- cpp-project/microurl/src/ver3/MicroUrlService.cpp
+++ cpp-project/microurl/src/ver3/MicroUrlService.cpp
@@ -14,6 +14,15 @@ MicroUrlService::MicroUrlService(std::unique_ptr<IIdGenerator> generator)
 
 }
 
+std::string MicroUrlService::MakeMicroUrl(const char* url)
+{
+	auto id = m_idGenerator->GenerateId(url);
+	auto secret = Ext::Shortener::idToShortURL(id);
+	auto microUrl = string("https://micro.url/") + secret;
+	m_idToUrl[id] = { url, microUrl, 0 };
+	return microUrl;
+}
+
 // { autofold
 
 MicroUrlService::~MicroUrlService() = default;
@@ -32,14 +41,5 @@ UrlInfo MicroUrlService::Stats(const std::string& microUrl) const
 	string strMicro(microUrl);
 	auto secret = strMicro.substr(strMicro.find_last_of('/') + 1);
 	return m_idToUrl.find(Ext::Shortener::shortURLtoID(secret.c_str()))->second;
-}
-
-std::string MicroUrlService::MakeMicroUrl(const char* url)
-{
-	auto id = m_idGenerator->GenerateId(url);
-	auto secret = Ext::Shortener::idToShortURL(id);
-	auto microUrl = string("https://micro.url/") + secret;
-	m_idToUrl[id] = { url, microUrl, 0 };
-	return microUrl;
 }
 // }
